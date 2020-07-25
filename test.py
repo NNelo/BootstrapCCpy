ca1754389a1fc560b9a3885234d2001c62d43b1c
NNelo <1608891@ucc.edu.ar>
2020-07-25 00:33:45 -0300

    Deleting test files

--- test.py
+++
@@ -1,6 +0,0 @@
-from BootstrapCCpy import BootstrapCCpy as bcc
-from sklearn.cluster import KMeans
-
-if __name__ == '__main__':
-    CC = bcc(cluster=KMeans().__class__, K=10, B=250, n_cores=4)
-    print("main")
